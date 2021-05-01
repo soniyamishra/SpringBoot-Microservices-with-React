fbcda1cdb478027c53e1c40fff6abf9f7b1d7c18
soniyamishra <53016225+soniyamishra@users.noreply.github.com>
2021-05-01 15:25:12 +0530

    update in employee client dashboard

---
+++ EmployeeSearchServiceClientDashboardApplication/src/main/java/com/capg/EmployeeInfo.java
@@ -0,0 +1,55 @@
+package com.capg;
+
+public class EmployeeInfo {
+
+	private int eid;
+
+	private String name;
+
+	private String designation;
+	
+	private String emailId;
+	
+	
+	
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	
+	
+	
+
+	
+}
