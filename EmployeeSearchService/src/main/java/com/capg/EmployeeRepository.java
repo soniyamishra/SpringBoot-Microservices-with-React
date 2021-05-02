c813f6ce36f9eeb2c4e3cd609fd4ea8c057c59bc
soniyamishra <53016225+soniyamishra@users.noreply.github.com>
2021-05-02 18:29:45 +0530

    update in employee search service

---
+++ EmployeeSearchService/src/main/java/com/capg/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.capg;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
---
+++ EmployeeSearchService/src/test/java/com/capg/EmployeeSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
